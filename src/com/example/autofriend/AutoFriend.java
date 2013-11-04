d4da0f7b1edb97a1864789c5db3bc944d5eb4cf1
Michael Perng <michaelperng@gmail.com>
2013-11-04 17:42:49 -0600

    Added non-working button and corresponding handlers in main activity

--- src/com/example/autofriend/AutoFriend.java
+++ src/com/example/autofriend/AutoFriend.java
@@ -7,16 +7,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ToggleButton;
 
 public class AutoFriend extends Activity {
 	
 	private static final String TAG = "AutoFriend";
 	private static final String serviceName = "AUTO_FRIEND";
+	private static Intent service;
+	private static boolean running = false;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		Log.v(TAG, "onCreate");
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 	}
 	
 	@Override
@@ -32,19 +36,21 @@ public class AutoFriend extends Activity {
 	
 	private void stopAutoFriend(View v) {
 		Log.v(TAG, "stopAutoFriend");
-		Intent service = new Intent(this, AutoFriendService.class);
+		//Intent 
+		service = new Intent(this, AutoFriendService.class);
 		boolean stopped = stopService(service);
 		Log.d(TAG, "service stopped: " + stopped);
 	}
 	
 	private void startAutoFriendService() {
 		Log.v(TAG, "startAutoFriendService");
-		boolean running = isAutoFriendServiceRunning();
+		running = isAutoFriendServiceRunning();
 		Log.d(TAG, "service running: " + running);
 		
 		if (!running) {
 			try {
-				Intent service = new Intent(this, AutoFriendService.class);
+				//Intent 
+				service = new Intent(this, AutoFriendService.class);
 				startService(service);
 			} catch (Exception e) {
 				Log.e("onCreate", "service creation problem", e);
@@ -66,4 +72,17 @@ public class AutoFriend extends Activity {
 		
 		return result;
 	}
+	
+	// serviceToggle click event handler
+	public void onServiceToggleClicked(View view) {
+		Log.v(TAG, "onServiceToggle");
+
+		boolean on = ((ToggleButton) view).isChecked();
+		if (on) {
+			startAutoFriend(view);
+		} else {
+			stopAutoFriend(view);
+		}
+	}
+
 }
