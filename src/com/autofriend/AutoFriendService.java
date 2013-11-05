ea237f5887d414c0d2ad68feccd61e9a5eb90c4b
unknown <yiyu.liu@live.com>
2013-11-04 23:16:51 -0600

    added cleverbot component

--- src/com/autofriend/AutoFriendService.java
+++ src/com/autofriend/AutoFriendService.java
@@ -1,5 +1,10 @@
 package com.autofriend;
 
+import com.google.code.chatterbotapi.ChatterBot;
+import com.google.code.chatterbotapi.ChatterBotFactory;
+import com.google.code.chatterbotapi.ChatterBotSession;
+import com.google.code.chatterbotapi.ChatterBotType;
+
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -14,78 +19,89 @@ import android.telephony.SmsMessage;
 import android.util.Log;
 
 public class AutoFriendService extends Service {
-	
+
 	private static final String TAG = "AutoFriendService";
 	private static final String RECEIVED_ACTION
-		= "android.provider.Telephony.SMS_RECEIVED";
+	= "android.provider.Telephony.SMS_RECEIVED";
 	private static final String SENT_ACTION = "SENT_SMS";
 	private static final String DELIVERED_ACTION = "DELIVERED_SMS";
-	
+	private final ChatterBotType type = ChatterBotType.CLEVERBOT;
+	private static ChatterBotFactory botFactory;
+	private static ChatterBot bot;
+	private static ChatterBotSession session;
+
 	private String requester;
-	
+
 	private BroadcastReceiver receiver = new BroadcastReceiver() {
 		@Override
 		public void onReceive(Context c, Intent in) {
 			Log.v(TAG, "receiver:onReceive");
-			
+
 			if (in.getAction().equals(RECEIVED_ACTION)) {
 				Bundle bundle = in.getExtras();
-				
+
 				if (bundle != null) {
 					Object[] pdus = (Object[])bundle.get("pdus");
 					SmsMessage[] messages = new SmsMessage[pdus.length];
-					
+
 					for (int i = 0; i < pdus.length; ++i) {
 						messages[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
 					}
-					
+
 					for (SmsMessage message: messages) {
 						requestReceived(message.getOriginatingAddress());
 					}
-					
+
 					/* need to pass the received message */
 					respond();
 				}
 			}
 		}
 	};
-	
+
 	private BroadcastReceiver sender = new BroadcastReceiver() {
-    	@Override
-    	public void onReceive(Context c, Intent i) {
-    		Log.v(TAG, "sender:onReceive");
-    		
-    		if(i.getAction().equals(SENT_ACTION)) {    		
-    			if(getResultCode() != Activity.RESULT_OK) {
-    				String reciptent = i.getStringExtra("recipient");
-    				requestReceived(reciptent);
-    			}
-    		}
-    	}
-    };
-	
+		@Override
+		public void onReceive(Context c, Intent i) {
+			Log.v(TAG, "sender:onReceive");
+
+			if(i.getAction().equals(SENT_ACTION)) {    		
+				if(getResultCode() != Activity.RESULT_OK) {
+					String reciptent = i.getStringExtra("recipient");
+					requestReceived(reciptent);
+				}
+			}
+		}
+	};
+
 	@Override
 	public void onCreate() {
 		Log.d(TAG, "onCreate");
 		super.onCreate();
-		
+
 		IntentFilter receiverFilter = new IntentFilter(RECEIVED_ACTION);
 		registerReceiver(receiver, receiverFilter);
-		
+
 		IntentFilter senderFilter = new IntentFilter(SENT_ACTION);
 		registerReceiver(sender, senderFilter);
+
+		botFactory = new ChatterBotFactory();
+		try {
+			bot = botFactory.create(type);
+		} catch (Exception e) {
+		}
+		session = bot.createSession();
 	}
-	
+
 	@Override
 	public int onStartCommand(Intent inten, int flags, int startId) {
 		return START_NOT_STICKY;
 	}
-	
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		Log.d(TAG, "onDestroy");
@@ -93,25 +109,29 @@ public class AutoFriendService extends Service {
 		unregisterReceiver(receiver);
 		unregisterReceiver(sender);
 	}
-	
+
 	public void requestReceived(String f) {
-    	Log.v(TAG, "requestReceived");
-    	requester = f;
-    }
-	
-	private void respond() {
+		Log.v(TAG, "requestReceived");
+		requester = f;
+	}
+
+	private void respond(String message) {
 		Log.v(TAG, "respond");
 		/* set reply to CleverBot's respond */
-		String reply = "Hello, World";
-		
-        SmsManager sms = SmsManager.getDefault();
-    	Intent sentIn = new Intent(SENT_ACTION);
-    	PendingIntent sentPIn = PendingIntent.getBroadcast(this, 0, sentIn, 0);
+		String reply = "";
+		try {
+			session.think(message);
+		} catch (Exception e) {
+		}
 
-    	Intent deliverIn = new Intent(DELIVERED_ACTION);
-    	PendingIntent deliverPIn = PendingIntent.getBroadcast(this, 0, deliverIn, 0);
-       
-        sms.sendTextMessage(requester, null, reply, sentPIn, deliverPIn);
+		SmsManager sms = SmsManager.getDefault();
+		Intent sentIn = new Intent(SENT_ACTION);
+		PendingIntent sentPIn = PendingIntent.getBroadcast(this, 0, sentIn, 0);
 
-    }
+		Intent deliverIn = new Intent(DELIVERED_ACTION);
+		PendingIntent deliverPIn = PendingIntent.getBroadcast(this, 0, deliverIn, 0);
+
+		sms.sendTextMessage(requester, null, reply, sentPIn, deliverPIn);
+
+	}
 }
