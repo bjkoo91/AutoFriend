aee7821f3b2bba37e537e60f0b3eacf6d19b810d
Bon <bjkoo91@gmail.com>
2013-11-05 02:25:00 -0600

    resolved warnings

--- src/com/autofriend/AutoFriend.java
+++ src/com/autofriend/AutoFriend.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
