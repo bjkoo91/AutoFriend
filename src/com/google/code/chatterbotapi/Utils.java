4a0ebe48b946b94f62c790c554bc4d878974174d
Bon <bjkoo91@gmail.com>
2013-11-05 02:15:21 -0600

    fixed networking connection exception with AsyncTask

--- src/com/google/code/chatterbotapi/Utils.java
+++ src/com/google/code/chatterbotapi/Utils.java
@@ -12,13 +12,18 @@ import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.security.MessageDigest;
 import java.util.Map;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
+
 import org.w3c.dom.Document;
+
+import android.os.AsyncTask;
+import android.util.Log;
 
 /*
     chatter-bot-api
@@ -60,23 +65,9 @@ class Utils {
     }
     
     public static String post(String url, Map<String, String> parameters) throws Exception {
-        URLConnection connection = new URL(url).openConnection();
-        connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36");
-        connection.setDoOutput(true);
-        connection.setDoInput(true);
-        OutputStreamWriter osw = new OutputStreamWriter(connection.getOutputStream());
-        osw.write(parametersToWWWFormURLEncoded(parameters));
-        osw.flush();
-        osw.close();
-        Reader r = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringWriter w = new StringWriter();
-        char[] buffer = new char[1024];
-        int n = 0;
-        while ((n = r.read(buffer)) != -1) {
-            w.write(buffer, 0, n);
-        }
-        r.close();
-        return w.toString();
+    	ConnectionTask t = new Utils().new ConnectionTask();
+        Input in = new Utils().new Input(url, parameters);
+        return t.execute(in).get();
     }
     
     public static String xPathSearch(String input, String expression) throws Exception {
@@ -91,5 +82,47 @@ class Utils {
     public static String stringAtIndex(String[] strings, int index) {
         if (index >= strings.length) return "";
         return strings[index];
+    }
+    
+    protected class Input {
+        protected String url;
+        protected Map<String, String> parameters;
+        
+        public Input(String _url, Map<String, String> _parameters) {
+            url = _url;
+            parameters = _parameters;
+        }
+    }
+    
+    protected class ConnectionTask extends AsyncTask<Input, Void, String> {
+    	@Override
+        protected String doInBackground(Input... inputs) {
+            try {
+                Input in = inputs[0];
+                String url = in.url;
+                Map<String, String> parameters = in.parameters;
+                
+                URLConnection connection = new URL(url).openConnection();
+                connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36");
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+                OutputStreamWriter osw = new OutputStreamWriter(connection.getOutputStream());
+                osw.write(parametersToWWWFormURLEncoded(parameters));
+                osw.flush();
+                osw.close();
+                Reader r = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringWriter w = new StringWriter();
+                char[] buffer = new char[1024];
+                int n = 0;
+                while ((n = r.read(buffer)) != -1) {
+                    w.write(buffer, 0, n);
+                }
+                r.close();
+                return w.toString();
+            } catch (Exception e) {
+            	Log.e("ConnectionTask", "connection task problem", e);
+            	return null;
+            }
+        }
     }
 }
