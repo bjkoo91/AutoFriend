5b1a32225a74075fe1b0255622fe1a50d0f962c8
Michael Perng <michaelperng@gmail.com>
2013-11-04 18:27:05 -0600

    Added save state for button, other edits

--- src/com/example/autofriend/AutoFriend.java
+++ src/com/example/autofriend/AutoFriend.java
@@ -4,29 +4,49 @@ import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ToggleButton;
 
+
 public class AutoFriend extends Activity {
 	
 	private static final String TAG = "AutoFriend";
 	private static final String serviceName = "AUTO_FRIEND";
-	private static Intent service;
-	private static boolean running = false;
+	private static boolean mServiceOn;
+	private static SharedPreferences mPrefs;
+	private static ToggleButton mServiceToggleButton;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		Log.v(TAG, "onCreate");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// get toggle button state
+		mPrefs = getSharedPreferences("ttt_pref", MODE_PRIVATE);
+		mServiceOn = mPrefs.getBoolean("mServiceOn", false);
+		mServiceToggleButton = (ToggleButton) findViewById(R.id.serviceToggleButton);
+		mServiceToggleButton.setChecked(mServiceOn);
 	}
 	
 	@Override
 	public void onResume() {
 		Log.v(TAG, "onResume");
 		super.onResume();
+	}
+	
+	@Override
+	public void onStop() {
+		Log.v(TAG, "onStop");
+		super.onStop();
+		
+		// Save the toggle button state
+		SharedPreferences.Editor ed = mPrefs.edit();
+		ed.putBoolean("mServiceOn", mServiceOn);
+		ed.commit();
 	}
 	
 	private void startAutoFriend(View v) {
@@ -36,26 +56,27 @@ public class AutoFriend extends Activity {
 	
 	private void stopAutoFriend(View v) {
 		Log.v(TAG, "stopAutoFriend");
-		//Intent 
-		service = new Intent(this, AutoFriendService.class);
+		Intent service = new Intent(this, AutoFriendService.class);
 		boolean stopped = stopService(service);
 		Log.d(TAG, "service stopped: " + stopped);
+		mServiceOn = !stopped;
 	}
 	
 	private void startAutoFriendService() {
 		Log.v(TAG, "startAutoFriendService");
-		running = isAutoFriendServiceRunning();
+		boolean running = isAutoFriendServiceRunning();
 		Log.d(TAG, "service running: " + running);
 		
 		if (!running) {
 			try {
-				//Intent 
-				service = new Intent(this, AutoFriendService.class);
+				Intent service = new Intent(this, AutoFriendService.class);
 				startService(service);
+				running = isAutoFriendServiceRunning();
 			} catch (Exception e) {
 				Log.e("onCreate", "service creation problem", e);
 			}
 		}
+		mServiceOn = running;
 	}
 	
 	private boolean isAutoFriendServiceRunning() {
@@ -79,8 +100,10 @@ public class AutoFriend extends Activity {
 
 		boolean on = ((ToggleButton) view).isChecked();
 		if (on) {
+			Log.d(TAG,"button toggled on");
 			startAutoFriend(view);
 		} else {
+			Log.d(TAG,"button toggled off");
 			stopAutoFriend(view);
 		}
 	}
