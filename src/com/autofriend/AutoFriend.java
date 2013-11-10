6adf83358475d33e50cca991e434669ad9cab4dd
unknown <yiyu.liu@live.com>
2013-11-10 15:09:15 -0600

    changed the toggle button to a switch and moved it into the action bar

--- src/com/autofriend/AutoFriend.java
+++ src/com/autofriend/AutoFriend.java
@@ -6,44 +6,69 @@ import android.app.ActivityManager.RunningServiceInfo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
-import android.widget.ToggleButton;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class AutoFriend extends Activity {
-	
+
 	private static final String TAG = "AutoFriend";
 	private static final String serviceName = "com.autofriend.AutoFriendService";
 	private static boolean mServiceOn;
-	private static ToggleButton mServiceToggleButton;
-	
+	private static Switch serviceSwitch;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		Log.v(TAG, "onCreate");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		mServiceOn = isAutoFriendServiceRunning();
-		mServiceToggleButton = (ToggleButton) findViewById(R.id.serviceToggleButton);
-		mServiceToggleButton.setChecked(mServiceOn);
 	}
-	
+
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.action_bar_menu, menu);
+		serviceSwitch = (Switch) menu.findItem(R.id.serviceSwitch).getActionView();
+		serviceSwitch.setChecked(mServiceOn);
+		serviceSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				Log.v(TAG, "onServiceToggleClicked");
+
+				boolean on = ((Switch) buttonView).isChecked();
+				if (on) {
+					Log.d(TAG, "button toggled on");
+					startAutoFriend(buttonView);
+				} else {
+					Log.d(TAG, "button toggled off");
+					stopAutoFriend(buttonView);
+				}
+			}
+		});
+		return true;
+	}
+
 	@Override
 	public void onResume() {
 		Log.v(TAG, "onResume");
 		super.onResume();
 	}
-	
+
 	@Override
 	public void onStop() {
 		Log.v(TAG, "onStop");
 		super.onStop();
 	}
-	
+
 	private void startAutoFriend(View v) {
 		Log.v(TAG, "startAutoFriend");
 		startAutoFriendService();
 	}
-	
+
 	private void stopAutoFriend(View v) {
 		Log.v(TAG, "stopAutoFriend");
 		Intent service = new Intent(this, AutoFriendService.class);
@@ -51,7 +76,7 @@ public class AutoFriend extends Activity {
 		Log.d(TAG, "service stopped: " + stopped);
 		mServiceOn = !stopped;
 	}
-	
+
 	/*
 	 * Start the AutoFriend Service if the service is not already running
 	 */
@@ -59,7 +84,7 @@ public class AutoFriend extends Activity {
 		Log.v(TAG, "startAutoFriendService");
 		boolean running = isAutoFriendServiceRunning();
 		Log.d(TAG, "service running: " + running);
-		
+
 		if (!running) {
 			try {
 				Intent service = new Intent(this, AutoFriendService.class);
@@ -72,7 +97,7 @@ public class AutoFriend extends Activity {
 		}
 		mServiceOn = running;
 	}
-	
+
 	/*
 	 * Check if AutoFriendService is already running or not
 	 */
@@ -80,27 +105,13 @@ public class AutoFriend extends Activity {
 		Log.v(TAG, "isServiceRunning");
 		ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
 		boolean result = false;
-		
+
 		for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
 			if (serviceName.equals(service.service.getClassName())) {
 				result = true;
 			}
 		}
-		
+
 		return result;
 	}
-	
-	public void onServiceToggleClicked(View view) {
-		Log.v(TAG, "onServiceToggleClicked");
-
-		boolean on = ((ToggleButton) view).isChecked();
-		if (on) {
-			Log.d(TAG, "button toggled on");
-			startAutoFriend(view);
-		} else {
-			Log.d(TAG, "button toggled off");
-			stopAutoFriend(view);
-		}
-	}
-
 }
