9b1571c455ffc8f77e46f39d36bce86f02bb32ab
Michael Perng <michaelperng@gmail.com>
2013-11-12 12:32:00 -0600

    semi-working contacts list implementation

--- src/com/autofriend/AutoFriend.java
+++ src/com/autofriend/AutoFriend.java
@@ -1,17 +1,33 @@
 package com.autofriend;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.Switch;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AutoFriend extends Activity {
 
@@ -19,6 +35,10 @@ public class AutoFriend extends Activity {
 	private static final String serviceName = "com.autofriend.AutoFriendService";
 	private static boolean mServiceOn;
 	private static Switch serviceSwitch;
+	private ListView mContactsListView;
+	private ContactsListAdapter mContactsListAdapter = null;
+	private ArrayList<Contact> mContacts;
+    private static SparseBooleanArray mSelectedContacts = new SparseBooleanArray(); 
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -26,7 +46,14 @@ public class AutoFriend extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		mContacts = new ArrayList<Contact>();
 		mServiceOn = isAutoFriendServiceRunning();
+		mContactsListView = (ListView) findViewById(R.id.contact_list);
+//		ViewHolder holder = new ViewHolder();
+//		holder.displayName = (TextView) findViewById(R.id.display_name);
+//		holder.setService = (CheckBox) findViewById(R.id.set_service);
+//		mContactsListView.setTag(holder);
+		new LoadContacts().execute();
 	}
 
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -114,4 +141,153 @@ public class AutoFriend extends Activity {
 
 		return result;
 	}
+	
+    /*
+     * Populate the contact list
+     */
+    private void getContactsList() {
+		Log.v(TAG, "getContactsList");
+        // Build adapter with contact entries
+    	String [] projection = new String[] {
+    				ContactsContract.Contacts._ID,
+    				ContactsContract.Contacts.DISPLAY_NAME 
+    	};
+        Cursor cursor = getContentResolver().query(	
+        		ContactsContract.Contacts.CONTENT_URI,
+        		projection,
+        		null,
+        		null,
+        		ContactsContract.Contacts.DISPLAY_NAME);
+        while(cursor.moveToNext()) {
+        	String contact_name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+        	boolean checked = false;
+        	Contact contact = new Contact(contact_name, checked);
+        	mContacts.add(contact);
+        }
+        cursor.close();
+        Log.d(TAG,"mContacts: "+Arrays.toString(mContacts.toArray()));
+//        String [] columns = new String [] { ContactsContract.Data.DISPLAY_NAME };
+//        int [] textViewIds = { R.id.display_name };
+//		Log.d(TAG, "ContactListAdapter");
+//        ListAdapter adapter = new ContactsListAdapter(this, R.layout.contact_entry, cursor, columns, textViewIds);
+//        return adapter;
+        //mContactListView.setAdapter(adapter);
+    }
+    
+    private ContactsListAdapter getContactsListAdapter() {
+		Log.v(TAG, "getContactsListAdapter");
+    	return new ContactsListAdapter(this, R.layout.contact_entry, mContacts);
+    }
+    
+
+    
+    private static class ContactsListAdapter extends ArrayAdapter<Contact> {
+    	ArrayList<Contact> contacts;
+    	Context c;
+
+    	private ContactsListAdapter(Context c, int layout, ArrayList<Contact> contacts) {
+    		super(c, layout, contacts);
+    		this.c = c;
+    		this.contacts = contacts;
+    	}
+    	
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+        		Log.v(TAG,"ContactsListAdapter.getView");
+                View view = convertView;
+                if (view == null) {
+                    LayoutInflater vi = (LayoutInflater)c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    view = vi.inflate(R.layout.contact_entry, null);
+                }
+                Contact contact = contacts.get(position);
+                Log.d(TAG,"got contact?");
+                if (contact != null) {
+                        CheckBox nameCheckBox = (CheckBox) view.findViewById(R.id.set_service);
+                        nameCheckBox.setChecked(mSelectedContacts.get(position));
+                        if (nameCheckBox != null) {
+                            nameCheckBox.setText(contact.getName());
+                        }
+                        nameCheckBox.setOnClickListener(new OnItemClickListener(position,nameCheckBox.getText(),nameCheckBox));
+                }
+                return view;
+        }
+        
+        // OnClickListener for individual checkboxes
+        class OnItemClickListener implements OnClickListener{          
+            private int position;
+            private CharSequence text;
+            private CheckBox checkBox;
+            OnItemClickListener(int position, CharSequence text,CheckBox checkBox){
+                    this.position = position;
+                    this.text = text;
+                    this.checkBox = checkBox;
+                    Log.v(TAG,"onItemClickListener "+position +" and text "+text);
+            }
+            
+            @Override
+            public void onClick(View arg0) {
+            	// check if the position was selected before. If not, add it
+            	// to selected contacts list; else, remove it
+            	if(!mSelectedContacts.get(position))
+            		mSelectedContacts.append(position, true);
+            	else
+            		mSelectedContacts.delete(position);
+                Log.d(TAG,"Clicked "+position +" and text "+text);
+            }              
+        }
+    }
+    
+    public static class ViewHolder {
+    	TextView displayName;
+    	CheckBox setService;
+    }
+    
+    public class LoadContacts extends AsyncTask<Void, Void, Void> {
+    	// private ViewHolder viewHolder;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Do Dialog stuff here
+            Log.v(TAG,"LoadContacts");
+        }
+
+        protected Void doInBackground(Void... args) {
+        	// viewHolder = args[0];
+            // Put your implementation to retrieve contacts here
+            Log.v(TAG,"doInBackground");
+        	getContactsList();
+        	return null;
+        }
+
+        protected void onPostExecute(Void result) {
+            Log.v(TAG,"onPostExecute");
+            // Dismiss Dialog
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    Log.d(TAG,"onPostExecute.run");
+                    // Create list adapter and notify it
+                	mContactsListAdapter = getContactsListAdapter();
+                	mContactsListAdapter.notifyDataSetChanged();
+            		mContactsListView.setAdapter(mContactsListAdapter);
+                }
+            });
+            return;
+        }
+    }
+    
+    public class Contact {
+    	private String name;
+    	private boolean checked;
+    	public Contact(String name, boolean checked) {
+    		this.name = name;
+    		this.checked = checked;
+    	}
+    	public String getName() {
+    		return name;
+    	}
+    	public boolean getChecked() {
+    		return checked;
+    	}
+    }
+
 }
