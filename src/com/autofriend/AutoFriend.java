2a0c688eb36f64c44990f8d886469ea7e3323ef5
unknown <yiyu.liu@live.com>
2013-12-01 20:51:46 -0600

    broken code for debugging

--- src/com/autofriend/AutoFriend.java
+++ src/com/autofriend/AutoFriend.java
@@ -3,6 +3,7 @@ package com.autofriend;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -43,10 +44,9 @@ public class AutoFriend extends Activity {
 	private static Switch serviceSwitch;
 	private ListView mContactsListView;
 	private ContactsListAdapter mContactsListAdapter = null;
-	private ArrayList<Contact> mContacts;
-    private static SparseBooleanArray mSelectedContacts = new SparseBooleanArray(); 
-    private static Map<String, String> mNameToNumber;
-    private SharedPreferences mPrefs;
+	private static ArrayList<Contact> mContacts;
+	private SharedPreferences mPrefs;
+	private static HashSet<String> numbers;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -57,25 +57,12 @@ public class AutoFriend extends Activity {
 		mContacts = new ArrayList<Contact>();
 		mServiceOn = isAutoFriendServiceRunning();
 		mContactsListView = (ListView) findViewById(R.id.contact_list);
-		mNameToNumber = new HashMap<String, String>();
-//		ViewHolder holder = new ViewHolder();
-//		holder.displayName = (TextView) findViewById(R.id.display_name);
-//		holder.setService = (CheckBox) findViewById(R.id.set_service);
-//		mContactsListView.setTag(holder);
 		mPrefs = getSharedPreferences("ttt_prefs", MODE_PRIVATE);
-		String json = mPrefs.getString("mNameToNumber", "");
-		if (!json.isEmpty()) {
-			try {
-			JSONObject jsonobj = new JSONObject(json);
-			 Iterator it = jsonobj.keys();
-			 while (it.hasNext()) {
-				 String key = (String) it.next();
-				 mNameToNumber.put(key, jsonobj.getString(key));
-			 }
-			} catch(JSONException e) {
-				e.printStackTrace();
-			}
-		}
+		
+		numbers = (HashSet<String>) mPrefs.getStringSet("numbers", null);
+		if (numbers == null) 
+			numbers = new HashSet<String>();
+		
 		new LoadContacts().execute();
 	}
 
@@ -114,7 +101,8 @@ public class AutoFriend extends Activity {
 		super.onStop();
 		SharedPreferences mPrefs = getSharedPreferences("ttt_prefs", MODE_PRIVATE);  
 		SharedPreferences.Editor ed = mPrefs.edit();
-		ed.putString("mNameToNumber", new JSONObject(mNameToNumber).toString());
+		//ed.putString("mNameToNumber", new JSONObject(mNumberToName).toString());
+		ed.putStringSet("numbers", numbers);
 		ed.commit();
 	}
 
@@ -168,155 +156,172 @@ public class AutoFriend extends Activity {
 
 		return result;
 	}
-	
-    /*
-     * Populate the contact list
-     */
-    private void getContactsList() {
+
+	/*
+	 * Populate the contact list
+	 */
+	private void getContactsList() {
 		Log.v(TAG, "getContactsList");
-        // Build adapter with contact entries
-    	String [] projection = new String[] {
-    				ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-    				ContactsContract.CommonDataKinds.Phone.NUMBER
-    	};
-        Cursor cursor = getContentResolver().query(	
-        		ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-        		projection,
-        		null,
-        		null,
-        		ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
-        while(cursor.moveToNext()) {
-        	String contact_name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-        	String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-        	mNameToNumber.put(contact_name, number);
-        	boolean checked = false; // need to change this to read from file
-        	Contact contact = new Contact(contact_name, number, checked);
-        	mContacts.add(contact);
-        }
-        cursor.close();
-        Log.d(TAG,"mContacts: "+Arrays.toString(mContacts.toArray()));
-    }
-    
-    private ContactsListAdapter getContactsListAdapter() {
+		// Build adapter with contact entries
+		String [] projection = new String[] {
+				ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+				ContactsContract.CommonDataKinds.Phone.NUMBER
+		};
+		Cursor cursor = getContentResolver().query(	
+				ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+				projection,
+				null,
+				null,
+				ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+		while(cursor.moveToNext()) {
+			String contact_name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+			String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+			number = number.replaceAll(" ", "");
+			number = number.replaceAll("-", "");
+			boolean checked = numbers.contains(number);
+			Contact contact = new Contact(contact_name, number, checked);
+			mContacts.add(contact);
+			Log.d("TEST", ""+ mContacts.size());
+		}
+		cursor.close();
+		Log.d(TAG,"mContacts: "+Arrays.toString(mContacts.toArray()));
+	}
+
+	private ContactsListAdapter getContactsListAdapter() {
 		Log.v(TAG, "getContactsListAdapter");
-    	return new ContactsListAdapter(this, R.layout.contact_entry, mContacts);
-    }
-    
+		return new ContactsListAdapter(this, R.layout.contact_entry, mContacts);
+	}
 
-    
-    private static class ContactsListAdapter extends ArrayAdapter<Contact> {
-    	ArrayList<Contact> contacts;
-    	Context c;
 
-    	private ContactsListAdapter(Context c, int layout, ArrayList<Contact> contacts) {
-    		super(c, layout, contacts);
-    		this.c = c;
-    		this.contacts = contacts;
-    	}
-    	
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-        		Log.v(TAG,"ContactsListAdapter.getView");
-                View view = convertView;
-                if (view == null) {
-                    LayoutInflater vi = (LayoutInflater)c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                    view = vi.inflate(R.layout.contact_entry, null);
-                }
-                Contact contact = contacts.get(position);
-                Log.d(TAG,"got contact?");
-                if (contact != null) {
-                        CheckBox nameCheckBox = (CheckBox) view.findViewById(R.id.set_service);
-                        nameCheckBox.setChecked(mSelectedContacts.get(position));
-                        if (nameCheckBox != null) {
-                            nameCheckBox.setText(contact.getName() + " " + contact.getNumber());
-                        }
-                        nameCheckBox.setOnClickListener(new OnItemClickListener(position,nameCheckBox.getText(),nameCheckBox));
-                }
-                return view;
-        }
-        
-        // OnClickListener for individual checkboxes
-        class OnItemClickListener implements OnClickListener{          
-            private int position;
-            private CharSequence text;
-            private CheckBox checkBox;
-            OnItemClickListener(int position, CharSequence text,CheckBox checkBox){
-                    this.position = position;
-                    this.text = text;
-                    this.checkBox = checkBox;
-                    Log.v(TAG,"onItemClickListener "+position +" and text "+text);
-            }
-            
-            @Override
-            public void onClick(View arg0) {
-            	// check if the position was selected before. If not, add it
-            	// to selected contacts list; else, remove it
-            	if(!mSelectedContacts.get(position))
-            		mSelectedContacts.append(position, true);
-            	else
-            		mSelectedContacts.delete(position);
-                Log.d(TAG,"Clicked "+position +" and text "+text);
-            }              
-        }
-    }
-    
-    public static class ViewHolder {
-    	TextView displayName;
-    	CheckBox setService;
-    }
-    
-    public class LoadContacts extends AsyncTask<Void, Void, Void> {
-    	// private ViewHolder viewHolder;
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            // Do Dialog stuff here
-            Log.v(TAG,"LoadContacts");
-        }
 
-        protected Void doInBackground(Void... args) {
-        	// viewHolder = args[0];
-            // Put your implementation to retrieve contacts here
-            Log.v(TAG,"doInBackground");
-        	getContactsList();
-        	return null;
-        }
+	private static class ContactsListAdapter extends ArrayAdapter<Contact> {
+		ArrayList<Contact> contacts;
+		Context c;
 
-        protected void onPostExecute(Void result) {
-            Log.v(TAG,"onPostExecute");
-            // Dismiss Dialog
-            runOnUiThread(new Runnable() {
-                public void run() {
-                    Log.d(TAG,"onPostExecute.run");
-                    // Create list adapter and notify it
-                	mContactsListAdapter = getContactsListAdapter();
-                	mContactsListAdapter.notifyDataSetChanged();
-            		mContactsListView.setAdapter(mContactsListAdapter);
-                }
-            });
-            return;
-        }
-    }
-    
-    public class Contact {
-    	private String name;
-    	private boolean checked;
-    	private String number;
-    
-    	public Contact(String name, String number, boolean checked) {
-    		this.name = name;
-    		this.number = number;
-    		this.checked = checked;
-    	}
-    	public String getName() {
-    		return name;
-    	}
-    	public boolean getChecked() {
-    		return checked;
-    	}
-    	public String getNumber() {
-    		return number;
-    	}
-    }
+		private ContactsListAdapter(Context c, int layout, ArrayList<Contact> contacts) {
+			super(c, layout, contacts);
+			this.c = c;
+			this.contacts = contacts;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			Log.v(TAG,"ContactsListAdapter.getView");
+			View view = convertView;
+			if (view == null) {
+				LayoutInflater vi = (LayoutInflater)c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				view = vi.inflate(R.layout.contact_entry, null);
+			}
+			Contact contact = contacts.get(position);
+			Log.d(TAG,"got contact?");
+			if (contact != null) {
+				CheckBox nameCheckBox = (CheckBox) view.findViewById(R.id.set_service);
+				//nameCheckBox.setChecked(mSelectedContacts.get(position));
+				nameCheckBox.setChecked(contacts.get(position).getChecked());
+				if (nameCheckBox != null) {
+					nameCheckBox.setText(contact.getName() + " " + contact.getNumber());
+				}
+				nameCheckBox.setOnClickListener(new OnItemClickListener(position,nameCheckBox.getText(),nameCheckBox));
+			}
+			return view;
+		}
+
+		// OnClickListener for individual checkboxes
+		class OnItemClickListener implements OnClickListener{          
+			private int position;
+			private CharSequence text;
+			private CheckBox checkBox;
+			OnItemClickListener(int position, CharSequence text,CheckBox checkBox){
+				this.position = position;
+				this.text = text;
+				this.checkBox = checkBox;
+				Log.v(TAG,"onItemClickListener "+position +" and text "+text);
+			}
+
+			@Override
+			public void onClick(View arg0) {
+				Contact clicked = mContacts.get(position);
+				if(clicked.getChecked()) {
+					numbers.remove(clicked.getNumber());
+					if(mServiceOn) {
+						AutoFriendService.removeNumber(clicked.getNumber());
+					}
+				} else {
+					numbers.add(clicked.getNumber());
+					if(mServiceOn) {
+						AutoFriendService.addNumber(clicked.getNumber());
+					}
+				}
+				clicked.toggle();
+				Log.d(TAG, "Toggled " + clicked.getChecked());
+			}              
+		}
+	}
+
+	public static class ViewHolder {
+		TextView displayName;
+		CheckBox setService;
+	}
+
+	public class LoadContacts extends AsyncTask<Void, Void, Void> {
+		// private ViewHolder viewHolder;
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			// Do Dialog stuff here
+			Log.v(TAG,"LoadContacts");
+		}
+
+		protected Void doInBackground(Void... args) {
+			// Put your implementation to retrieve contacts here
+			Log.v(TAG,"doInBackground");
+			getContactsList();
+			return null;
+		}
+
+		protected void onPostExecute(Void result) {
+			Log.v(TAG,"onPostExecute");
+			// Dismiss Dialog
+			runOnUiThread(new Runnable() {
+				public void run() {
+					Log.d(TAG,"onPostExecute.run");
+					// Create list adapter and notify it
+					mContactsListAdapter = getContactsListAdapter();
+					mContactsListAdapter.notifyDataSetChanged();
+					mContactsListView.setAdapter(mContactsListAdapter);
+				}
+			});
+			return;
+		}
+	}
+
+	public class Contact {
+		private String name;
+		private boolean checked;
+		private String number;
+
+		public Contact(String name, String number, boolean checked) {
+			this.name = name;
+			this.number = number;
+			this.checked = checked;
+		}
+		public String getName() {
+			return name;
+		}
+		public boolean getChecked() {
+			return checked;
+		}
+		public String getNumber() {
+			return number;
+		}
+		
+		public void toggle() {
+			checked = !checked;
+		}
+	}
+	
+	public static HashSet<String> getNumbers() {
+		return numbers;
+	}
 
 }
--- src/com/autofriend/AutoFriendService.java
+++ src/com/autofriend/AutoFriendService.java
@@ -1,6 +1,7 @@
 package com.autofriend;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 import android.app.Service;
 import android.content.BroadcastReceiver;
@@ -20,25 +21,33 @@ import com.google.code.chatterbotapi.ChatterBotType;
 public class AutoFriendService extends Service {
 
 	private static final String TAG = "AutoFriendService";
-	
+
 	private static final String SMS_RECEIVED
-		= "android.provider.Telephony.SMS_RECEIVED";
-	
+	= "android.provider.Telephony.SMS_RECEIVED";
+
 	private static final ChatterBotType type = ChatterBotType.CLEVERBOT;
 	private static ChatterBotFactory botFactory;
 	private static HashMap<String, ChatterBotSession> sessionMap;
-	
+	private static HashSet<String> numbers;
+
 	@Override
 	public void onCreate() {
 		Log.d(TAG, "onCreate");
 		super.onCreate();
-		
+
 		botFactory = new ChatterBotFactory();
 		sessionMap = new HashMap<String, ChatterBotSession>();
-		
+
 		IntentFilter receiverFilter = new IntentFilter(SMS_RECEIVED);
 		registerReceiver(receiver, receiverFilter);
-		
+
+		numbers = new HashSet<String>();
+		HashSet<String> temp = AutoFriend.getNumbers();
+		for (String number : temp) {
+			numbers.add(number);
+		}
+
+
 		MessageTimer.start();
 	}
 
@@ -52,12 +61,12 @@ public class AutoFriendService extends Service {
 	public void onDestroy() {
 		Log.d(TAG, "onDestroy");
 		super.onDestroy();
-		
+
 		unregisterReceiver(receiver);
-		
+
 		MessageTimer.stop();
 	}
-	
+
 	private BroadcastReceiver receiver = new BroadcastReceiver() {
 		@Override
 		public void onReceive(Context c, Intent in) {
@@ -69,7 +78,7 @@ public class AutoFriendService extends Service {
 				if (bundle != null) {
 					Object[] pdus = (Object[])bundle.get("pdus");
 					SmsMessage[] messages = new SmsMessage[pdus.length];
-					
+
 					for (int i = 0; i < pdus.length; ++i) {
 						messages[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
 					}
@@ -78,39 +87,52 @@ public class AutoFriendService extends Service {
 						/* 
 						 * Creating a unique ChatterBot session for each person and store them in the HashMap
 						 */
-						if(!sessionMap.containsKey(message.getOriginatingAddress())) {
-							try {
-								ChatterBot bot = botFactory.create(type);
-								ChatterBotSession session = bot.createSession();
-								sessionMap.put(message.getOriginatingAddress(), session);
-							} catch (Exception e) {
-								Log.e("onCreate", "error with bot creation", e);
+						if(isChecked(message.getOriginatingAddress())) {
+							if(!sessionMap.containsKey(message.getOriginatingAddress())) {
+								try {
+									ChatterBot bot = botFactory.create(type);
+									ChatterBotSession session = bot.createSession();
+									sessionMap.put(message.getOriginatingAddress(), session);
+								} catch (Exception e) {
+									Log.e("onCreate", "error with bot creation", e);
+								}
 							}
+							respond(message.getOriginatingAddress(), message.getDisplayMessageBody(), c);
 						}
-						
-						respond(message.getOriginatingAddress(), message.getDisplayMessageBody(), c);
 					}
 				}
 			}
 		}
 	};
-	
+
 	/*
 	 * Get CleverBot's respond to the received message
 	 * Add that respond to the message queue
 	 */
 	private void respond(String requester, String message, Context context) {
 		Log.v(TAG, "respond");
-		
+
 		try {
 			String reply = "Ok";
 			ChatterBotSession session = sessionMap.get(requester);
-			
+
 			reply = session.think(message);
-			
+
 			MessageTimer.addNewMessage(requester, reply, context);	
 		} catch (Exception e) {
 			Log.e("respond", "error with respond", e);
 		}
+	}
+
+	private boolean isChecked(String number) {
+		return numbers.contains(number);
+	}
+
+	public static void addNumber(String number) {
+		numbers.add(number);
+	}
+
+	public static void removeNumber(String number) {
+		numbers.remove(number);
 	}
 }
